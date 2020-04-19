bdc79d664cfd2afaa175d63f7ba1a249024e005e
Александр Будыльский <budylsky@adeptik.com>
2020-04-19 13:38:43 +0300

    Алгоритм выравнивания последовательностей

---
+++ src/Algorithms.DynamicProgramming/SequenceAlignmentAlgorithm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.DynamicProgramming
+{
+    /// <summary>
+    ///  Алгоритм выравнивания последовательностей
+    /// </summary>
+    /// <typeparam name="TElement">Тип элемента последовательности</typeparam>
+    public class SequenceAlignmentAlgorithm<TElement>
+        where TElement: IEquatable<TElement>
+    {
+        /// <summary>
+        /// Получение выравненных последовательностей
+        /// </summary>
+        /// <remarks>
+        ///     Если элемент пропущен, то будет указано дефолтное значение <typeparamref name="TElement"/>
+        /// </remarks>
+        /// <param name="first">Первая последовательностьы</param>
+        /// <param name="second">Вторая последовательность</param>
+        /// <param name="penalty">Штраф за несовпадение элементов</param>
+        /// <param name="gapPenalty">Штраф за разрыв цепочки (пропуск элемента)</param>
+        /// <returns>Выравненные последовательности, суммарное расстояние(штраф)</returns>
+        public (TElement[] First, TElement[] Second, int DistanceCost) GetAlignment(TElement[] first, TElement[] second, int penalty, int gapPenalty)
+        {
+            var solution = new int[first.Length + 1, second.Length + 1];
+            for (var i = 0; i <= first.Length; ++i)
+                solution[i, 0] = i * gapPenalty;
+            for (var j = 0; j <= second.Length; ++j)
+                solution[0, j] = j * gapPenalty;
+
+            for(var i = 1; i <= first.Length; ++i)
+            {
+                for(var j = 1; j <= second.Length; ++j)
+                {
+                    solution[i, j] = Math.Min(solution[i - 1, j - 1] + (first[i - 1].Equals(second[j - 1]) ? 0 : penalty), solution[i - 1, j] + gapPenalty);
+                    solution[i, j] = Math.Min(solution[i, j], solution[i, j - 1] + gapPenalty);
+                }
+            }
+
+            var currI = first.Length;
+            var currJ = second.Length;
+            var actualFirst = new List<TElement>(Math.Max(first.Length, second.Length)); //new TElement[Math.Max(first.Length, second.Length)];
+            var actualSecond = new List<TElement>(Math.Max(first.Length, second.Length));
+            while (currI > 0 && currJ > 0)
+            {
+                var currElementsEquals = first[currI - 1].Equals(second[currJ - 1]);
+                if (solution[currI, currJ] == solution[currI - 1, currJ - 1] + (currElementsEquals ? 0 : penalty))
+                {
+                    actualFirst.Add(first[currI - 1]);
+                    actualSecond.Add(second[currJ - 1]);
+                    currI -= 1;
+                    currJ -= 1;
+                }
+                else if (solution[currI, currJ] == solution[currI - 1, currJ] + gapPenalty)
+                {
+                    actualFirst.Add(first[currI - 1]);
+                    actualSecond.Add(default);
+                    currI -= 1;
+                }
+                else if (solution[currI, currJ] == solution[currI, currJ - 1] + gapPenalty)
+                {
+                    actualFirst.Add(default);
+                    actualSecond.Add(second[currJ - 1]);
+                    currJ -= 1;
+                }
+                else
+                    throw new ArgumentOutOfRangeException("Unsupported case");
+            }
+            actualFirst.Reverse();
+            actualSecond.Reverse();
+            return (actualFirst.ToArray(),  actualSecond.ToArray(), solution[first.Length, second.Length]);
+        }
+    }
+}
---
+++ test/Algorithms.DynamicProgramming.Test/SequenceAlignmentAlgorithmUnitTest.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Algorithms.DynamicProgramming.Tests
+{
+    [TestClass()]
+    public class SequenceAlignmentAlgorithmUnitTest
+    {
+        [TestMethod()]
+        public void GetAlignmentTest()
+        {
+            Case1();
+            Case2();
+        }
+
+        private class SequenceAlignmentInput
+        {
+            public string[] First { get; set; }
+
+            public string[] Second { get; set; }
+
+            public int Penalty { get; set; }
+
+            public int GapPenalty { get; set; }
+        }
+
+        private class SequenceAlignmentOutput
+        {
+            public string[] First { get; set; }
+
+            public string[] Second { get; set; }
+
+            public int Penalty { get; set; }
+        }
+
+        private void TestAlgorithm(SequenceAlignmentInput input, SequenceAlignmentOutput expected)
+        {
+            var caseName = new StackTrace().GetFrame(1).GetMethod().Name;
+            var algorithm = new SequenceAlignmentAlgorithm<string>();
+
+            (var actualFirst, var actualSecond, var actualPenalty) = algorithm.GetAlignment(input.First, input.Second, input.Penalty, input.GapPenalty);
+
+            Assert.AreEqual(expected.Penalty, actualPenalty, $"Invalid penalty for :{caseName}");
+            
+            void AssertArray(string[] expected, string[] actual, string message)
+            {
+                Assert.AreEqual(expected.Length, actual.Length, $"Invalid length for: {message} for case :{caseName}");
+                for (var i = 0; i < expected.Length; ++i)
+                    Assert.AreEqual(expected[i], actual[i], $"Invalid element for index:{i} for: {message} for case :{caseName}");
+            }
+            AssertArray(expected.First, actualFirst, "first");
+            AssertArray(expected.Second, actualSecond, "second");
+        }
+
+        private void Case1()
+            => TestAlgorithm(
+                new SequenceAlignmentInput
+                {
+                    First = new string[] { "A", "G", "G", "G", "C", "T" },
+                    Second = new string[] { "A", "G", "G", "C", "A" },
+                    GapPenalty = 1,
+                    Penalty = 1
+                },
+                new SequenceAlignmentOutput
+                {
+                    First = new string[] { "A", "G", "G", "G", "C", "T" },
+                    Second = new string[] { "A", null, "G", "G", "C", "A" },
+                    Penalty = 2
+                }
+                );
+
+        private void Case2()
+            => TestAlgorithm(
+                new SequenceAlignmentInput
+                {
+                    First = new string[] { "A", "G", "T", "A", "C", "G" },
+                    Second = new string[] { "A", "C", "A", "T", "A", "G" },
+                    GapPenalty = 1,
+                    Penalty = 2
+                },
+                new SequenceAlignmentOutput
+                {
+                    First = new string[] { "A", null, "G", "T", "A", "C", "G" },
+                    Second = new string[] { "A", "C", "A", "T", "A", null, "G" },
+                    Penalty = 4
+                }
+                );
+    }
+}
