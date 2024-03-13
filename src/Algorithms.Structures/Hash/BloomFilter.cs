5db86b8cf225c404aabd47d2dd3b21beb05d1b19
shurik88 <alexandrvictor88@mail.ru>
2024-03-13 20:10:05 +0300

    feat: добавление фильтра блума

---
+++ src/Algorithms.Structures/Hash/BloomFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Algorithms.Structures.Hash
+{
+    public class BloomFilter<T>
+        //where T : IEqualityComparer<T>
+    {
+        private readonly BitArray _array;
+        private readonly IEnumerable<Func<T, int>> _hashFunctions;
+
+        public BloomFilter(int expectedElements, double mistakesFreq)
+        {
+            var storageSize = Convert.ToInt32(-Math.Log(mistakesFreq) * expectedElements / Math.Pow(Math.Log(2), 2));
+            _array = new BitArray(storageSize, false);
+            var hashCount = Convert.ToInt32(storageSize * Math.Log(2) / expectedElements);
+            _hashFunctions = Enumerable.Range(0, hashCount).Select((x, i) => new Func<T, int>(y => y.GetHashCode() * (i + 1) % storageSize)).ToList();
+        }
+
+        public BloomFilter(int storageSize, int expectedElements)
+        {
+            _array= new BitArray(storageSize);
+            var hashCount = Convert.ToInt32(Math.Ceiling((storageSize * 1.0 / expectedElements) * Math.Log(2)));
+            _hashFunctions = Enumerable.Range(0, hashCount).Select((x, i) => new Func<T, int>(y => y.GetHashCode() * (i + 1) % storageSize)).ToList();
+        }
+
+        public void Add(T item)
+        {
+            foreach(var func in _hashFunctions)
+                _array[func(item)] = true;
+        }
+
+        public bool Contains(T item)
+        {
+            return _hashFunctions.All(func => _array[func(item)]);
+        }
+    }
+}
---
+++ test/Algorithms.Structures.Test/BloomFilterTest.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Structures.Hash;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Structures.Test
+{
+    [TestClass]
+    public class BloomFilterTest
+    {
+        [TestMethod]
+        public void BloomFilter() 
+        {
+            var filter = new BloomFilter<int>(100, 0.01);
+            filter.Contains(100).Should().BeFalse("100 not added before");
+            filter.Add(100);
+            filter.Contains(100).Should().BeTrue("100 added before");
+        }
+    }
+}
