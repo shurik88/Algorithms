65a3262640fc5f7dbbe269b223e9b279774a20d2
shurik88 <alexandrvictor88@mail.ru>
2024-03-18 20:04:02 +0300

    count min

---
+++ src/Algorithms.Structures/Hash/CountMin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Structures.Hash
+{
+    public class CountMin<T>
+    {
+        private readonly int[,] matrix;
+        private readonly Func<T, int>[] _hashFunctions;
+        public CountMin(double eps, double delta)
+        {
+            var width = Convert.ToInt32(Math.Ceiling(Math.E / eps));
+            var depth = Convert.ToInt32(Math.Ceiling(Math.Log(1.0/ delta)));
+            matrix = new int[depth, width];
+            _hashFunctions = Enumerable.Range(0, depth).Select((x, i) => new Func<T, int>(y => y.GetHashCode() * (i + 1) % width)).ToArray();
+        }
+
+        public void Update(T item, int count = 1)
+        {
+            for(var d = 0; d < _hashFunctions.Length; ++d)
+            {
+                var hash = _hashFunctions[d](item);
+                matrix[d, hash] += count;
+            }
+        }
+
+        public int GetCount(T item)
+        {
+            return _hashFunctions.Select((x, index) => matrix[index, x(item)]).Min();
+        }
+    }
+}
---
+++ test/Algorithms.Structures.Test/CountMinTest.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Structures.Hash;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Structures.Test
+{
+    [TestClass]
+    public class CountMinTest
+    {
+        [TestMethod]
+        public void CountMin()
+        {
+            var countMin = new CountMin<int>(0.0001, 0.01);
+            countMin.Update(3, 2);
+            countMin.GetCount(3).Should().Be(2, "added 2");
+        }
+
+    }
+}
